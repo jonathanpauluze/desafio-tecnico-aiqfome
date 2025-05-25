5a9601484c350b9b487ed5859b1e32650e1e48bd
Jonathan Pauluze <jonathanpauluze@gmail.com>
2025-05-25 13:38:14 -0300

    fix(footer): correcting color for dark mode

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -10,11 +10,11 @@ export function Footer() {
 
   return (
     <footer className="flex justify-center items-center flex-col gap-2 py-6 px-4 bg-neutral-100  dark:bg-gray-800">
-      <p className="text-sm text-center font-bold text-purple-700">
+      <p className="text-sm text-center font-bold text-purple-700 dark:text-purple-400">
         feito com 💜 em maringá-PR
       </p>
 
-      <p className="text-center font-bold text-purple-700">
+      <p className="text-center font-bold text-purple-700 dark:text-purple-400">
         aiqfome.com © 2007-2023 aiqfome LTDA . CNPJ: 09.186.786/0001-58
       </p>
     </footer>
